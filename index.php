89e6550cc5fc4a1c827aaa6b834bbde67451d0a5
s.shlyikov <mpeegy@ya.ru>
2019-08-17 17:28:45 +0300

    Каректировка фоток

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
     <title>Тест на распознавание лиц</title>
 </head>
 <body>
-    <a target="_blanck" href="img.php?name=_gdC7_MoYqY.jpg">
-        <img width="100" src="img.php?name=_gdC7_MoYqY.jpg">
+<h1>Тест на распознавание лиц</h1>
+    <a target="_blanck" href="img.php?name=hZpip1ADYvw.jpg">
+        <img width="300" src="img.php?name=hZpip1ADYvw.jpg">
     </a>
     <a target="_blanck" href="img.php?name=7RSJEPx7GHo.jpg">
-        <img width="100" src="img.php?name=7RSJEPx7GHo.jpg">
+        <img width="300" src="img.php?name=7RSJEPx7GHo.jpg">
     </a>
-    <a target="_blanck" href="img.php?name=hZpip1ADYvw.jpg">    
-        <img width="100" src="img.php?name=hZpip1ADYvw.jpg">
+    <a target="_blanck" href="img.php?name=IjYIOHBoFWg.jpg">    
+        <img width="300" src="img.php?name=IjYIOHBoFWg.jpg">
     </a>    
 </body>
 </html>
